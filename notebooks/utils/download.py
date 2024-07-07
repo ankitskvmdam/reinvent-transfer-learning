c17ec33133b5d629f8c1aa25a3206282595d112f
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-07-07 15:37:45 +0900

    feat: add util to download 3d molecules

--- notebooks/utils/download.py
+++ notebooks/utils/download.py
@@ -1,8 +1,11 @@
 import os
+import requests
+import json
+import urllib3
 
 
 def get_api_key() -> str:
-    """Read .`api_key` file in the current directory and return the api key.
+    """Read `.api_key` file in the current directory and return the api key.
 
     User needs to add `.api_key` in the current directory with api key. If it is
     not present then it will return an empty string.
@@ -20,3 +23,57 @@ def get_api_key() -> str:
 
     with open(api_key_path, "r") as reader:
         return reader.read()
+
+
+def download_3d_similar_molecules(query_smiles: str, save_path: str) -> bool:
+    """Download 3d similar molecules using cheese api.
+
+    Make sure that download directory exists.
+
+    Parameters
+    ----------
+    query_smiles : str
+        query molecule smiles
+    save_path : str
+        path to save the downloaded file.
+
+    Returns
+    -------
+    bool
+        True if downloaded successfully.
+    """
+
+    api_key = get_api_key()
+
+    if api_key == "":
+        print("Unable to get api key.")
+        return False
+
+    if not os.path.exists(os.path.dirname(save_path)):
+        print("Directory doesn't exist.")
+        return False
+
+    try:
+        urllib3.disable_warnings()
+        res = requests.get(
+            "https://api.cheese.themama.ai/molsearch",
+            {
+                "search_input": query_smiles,
+                "search_type": "fast",
+                "n_neighbors": 100,
+                "search_quality": "fast",
+                "descriptors": True,
+                "properties": True,
+                "filter_molecules": False,
+            },
+            headers={"Authorization": f"Bearer {api_key}"},
+            verify=False,
+        ).json()
+
+        with open(save_path, "w") as writer:
+            json.dump(res, writer)
+
+    except Exception as e:
+        print(e)
+
+    return False
